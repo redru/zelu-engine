bcd922381a20f8e624f7db962283c3712cf0036c
redru <redru@users.noreply.github.com>
2015-04-08 11:55:24 +0200

    Update PreUpdatePhaseAction.cpp

--- ZeluEngine/src/application/sources/PreUpdatePhaseAction.cpp
+++ ZeluEngine/src/application/sources/PreUpdatePhaseAction.cpp
@@ -1,1 +1,11 @@
+#include "../headers/PreUpdatePhaseAction.h"
 
+PreUpdatePhaseAction::PreUpdatePhaseAction(bool active) {
+	this->active = active;
+
+	engine = &ZeluEngine::getInstance();
+}
+
+void PreUpdatePhaseAction::execute() {
+	
+}
